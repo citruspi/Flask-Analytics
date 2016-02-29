c9884e607bca52d05cc1ee4c6d1562907613f5c7
Mihir Singh <git.service@mihirsingh.com>
2016-02-29 07:31:39 -0500

    Added programming language classifiers for Python 3.{0->5}

--- setup.py
+++ setup.py
@@ -22,6 +22,12 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Unix',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet'
     ]
 )
