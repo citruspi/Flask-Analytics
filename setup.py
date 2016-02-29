3e4a40f1b511b70ea814bc79b3d3417d8a6dac18
Mihir Singh <git.service@mihirsingh.com>
2016-02-29 07:23:31 -0500

    Removed the Python 2.6 programming language classifier

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet'
     ]
