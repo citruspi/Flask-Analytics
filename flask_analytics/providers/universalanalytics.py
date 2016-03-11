7d5354c0138007608ef459a8d1f57c4f00c4de29
John Groves <johngroves@J0hns-Macbook-Pr0.local>
2016-03-11 14:25:15 -0800

    Fix jinja conflict

--- flask_analytics/providers/universalanalytics.py
+++ flask_analytics/providers/universalanalytics.py
@@ -10,21 +10,23 @@ class UniversalAnalytics(BaseProvider):
 
     @property
     def template(self):
-        return """<script>
-  !(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        return """<script>(function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
+  (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  }})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', '{account}', 'auto');
   ga('send', 'pageview');
 
 </script>"""
 
-
-
     @property
     def source(self):
         if self.account is None:
             return None
         return self.template.format(account=self.account)
+
+
+
+
+  
