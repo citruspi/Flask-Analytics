03b7e930f47778a9087187cc74bc4c16526ba8fd
Mihir Singh <git.service@mihirsingh.com>
2016-10-11 18:14:35 -0500

    Bumped the package version from 0.5.6 to 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Flask-Analytics",
-    version="0.5.6",
+    version="0.6.0",
     author="Mihir Singh (@citruspi)",
     author_email="hello@mihirsingh.com",
     description="Analytics snippets generator extension for the Flask framework",
